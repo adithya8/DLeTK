19c126c076d1a76d970d170110a291436ac29404
Adithya V Ganesan <v.adithyaganesan@gmail.com>
2023-06-27 19:02:23 -0400

    added new blocks to add

--- trainer/model/pytorch/block.py
+++ trainer/model/pytorch/block.py
@@ -35,5 +35,22 @@ class Prod(PytorchBlock):
         
     def forward(self, *args):
         return torch.prod(args, dim=self.dim)
+    
+class Matmul(PytorchBlock):
+    def __init__(self) -> None:
+        super().__init__()
+    
+    def forward(self):
+        raise NotImplementedError("Matmul is not implemented yet.")
 
 ############################################
+
+class HFTransformers(PytorchBlock):
+    def __init__(self, model_name: str, **kwargs) -> None:
+        super().__init__()
+        self.model_name = model_name
+        self.kwargs = kwargs
+        self.model = None
+    
+    def forward(self):
+        raise NotImplementedError("HFTransformers is an abstract class. Please implement the forward method.")
