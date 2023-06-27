cca445e61bd087e9a40eaea9bb792ead354c8c04
Adithya V Ganesan <v.adithyaganesan@gmail.com>
2023-06-27 19:02:55 -0400

    Dynamic Pytorch nn.Module builder with 60% improvement in speed

--- trainer/model/pytorch/model_builder.py
+++ trainer/model/pytorch/model_builder.py
@@ -3,44 +3,57 @@ import torch.nn as nn
 from typing import Dict, List, Union
 
 from .block import *
-
-class PytorchModelBuilder(nn.Module):
+        
+class PytorchModelBuilder2(nn.Module):
     def __init__(self, model_architecture: Dict):
         super().__init__()
         self.model_architecture = model_architecture
-        # self.model = PytorchModelClass(name=self.model_architecture["network_name"]) 
         self.parse_model_architecture()
     
-    def forward(self, input_features_dict: Dict[str, torch.Tensor]):
-        """Forward pass of the model"""
-        
-        forward_output_features = set(self.model_architecture["output_names"])
-        output_features = {}
-        # Preparing the input features for the forward pass
-        for input_feature_name in self.model_architecture["input_names"]:
-            input_features_op = f"""{input_feature_name} = input_features_dict["{input_feature_name}"]"""
-            exec(input_features_op)
-            if input_feature_name in forward_output_features:
-                output_features[input_feature_name] = input_features_dict[input_feature_name]
-        
-        # Forward pass
-        for layer_name in self.model_architecture["architecture"].keys():
+    def forward(self, **kwargs):
+        """
+            Forward Pass of the model
+        """
+        forward_output_features = self.model_architecture["output_names"]
+        for layer_name in self.model_architecture['architecture'].keys():
             layer = self.get_submodule(layer_name)
             layer_input_names = self.model_architecture['architecture'][layer_name]["input_names"]
             layer_output_names = self.model_architecture['architecture'][layer_name]["output_names"]
-            layer_op = f"""{','.join(layer_output_names)} = layer({','.join(layer_input_names)});"""
-            exec(layer_op)
-            
-            # Storing the output features in return dictionary
-            for output in layer_output_names:
-                if output in forward_output_features:
-                    output_features[output] = eval(output)
-                
-        return output_features
+            layer_inputs = [kwargs[input_name] if input_name in kwargs else self.__dict__[input_name] for input_name in layer_input_names]
+            if len(layer_output_names) == 1: 
+                self.__dict__[layer_output_names[0]] = layer(*layer_inputs)
+            else:
+                layer_outputs = layer(*layer_inputs)
+                self.__dict__.update({output_name: output for output_name, output in zip(layer_output_names, layer_outputs)})
         
+        assert all([(output_name in self.__dict__ or output_name in kwargs) for output_name in forward_output_features]), \
+            'Output features %s not found in the forward pass' % ','.join([output_name for output_name in forward_output_features if (output_name not in self.__dict__ and output_name not in kwargs)])
+                                        
+        return {output_name: self.__dict__[output_name] if output_name in self.__dict__ else kwargs[output_name] for output_name in forward_output_features}
+
     def parse_model_architecture(self) -> None:
-        """Parses the model architecture and adds the layers to the model
         """
-        for layer_name, layer in self.model_architecture["architecture"].items():
+            Parses the model architecture and adds the layers to the model
+        """
+        for layer_name, layer in self.model_architecture['architecture'].items():
             self.add_module(layer_name, eval(f"{layer['module']}(**{layer['module_args']})"))
-        
+            
+    def save_model(self, model_dir_path: str) -> None:
+        """
+            Saves the model to the model_path
+        """
+        raise NotImplementedError("Saving model is not implemented for Pytorch Model Builder")
+    
+    def load_model(self, model_dir_path: str) -> None:
+        """
+            Loads the model from the model_path
+        """
+        raise NotImplementedError("Loading model is not implemented for Pytorch Model Builder")
+    
+    @staticmethod
+    def from_pretrained(model_dir_path: str):
+        """
+            Loads the model from the model_path
+        """
+        raise NotImplementedError("Loading model is not implemented for Pytorch Model Builder")
+    
